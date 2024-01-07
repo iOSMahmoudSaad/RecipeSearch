4c45fb228cba75677c0b7acf0e9966a280246a49
Mahmoud Saad <msaaad202020@gmail.com>
2024-01-07 13:59:24 +0200

    add New Api Key

--- RecipeSearch/Enumeration/Api.swift
+++ RecipeSearch/Enumeration/Api.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 enum API: String {
-    case app_id = "107a8c58"
-    case app_keys = "16af6483ec8bb67c42cc6e2ebfd4f048"
+    case app_id = "724cf8b9"
+    case app_keys = "77d32dd48721d4ae6446fdb21263765e"
     case baseURL = "https://api.edamam.com/search?"
 }
